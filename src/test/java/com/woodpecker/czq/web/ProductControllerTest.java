4234545dc73df3f7a7c80dda9c8c354bfa27105f
zeqiang.chen <1209120868@qq.com>
2019-08-06 16:29:07 +0800

    [zeqiang] feat: finish should_return_400_status_when_submit_invalid_request_of_create_product

--- src/test/java/com/woodpecker/czq/web/ProductControllerTest.java
+++ src/test/java/com/woodpecker/czq/web/ProductControllerTest.java
@@ -7,14 +7,23 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-class ProductControllerTest extends ApiTestBase{
+class ProductControllerTest extends ApiTestBase {
 
     @Test
     void should_create_the_product_and_return_201_status_when_submit_valid_request_of_create_product() throws Exception {
-            String createProductRequestJson = "{\"name\": \"牛奶\",\"price\": 3.2,\"imageUrl\": \"images/product/mick.jpg\"}";
+        String createProductRequestJson = "{\"name\": \"牛奶\",\"price\": 3.2,\"imageUrl\": \"images/product/mick.jpg\"}";
 
         getMockMvc().perform(post("/api/product")
                 .contentType(MediaType.APPLICATION_JSON_UTF8).content(createProductRequestJson))
                 .andExpect(status().is(201));
+    }
+
+    @Test
+    void should_return_400_status_when_submit_invalid_request_of_create_product() throws Exception {
+        String invalidRequestJson = "{\"price\": \"abc\", \"unit\": \"瓶\", \"imageUrl\": \"images/milk\"}";
+
+        getMockMvc().perform(post("/api/product")
+                .contentType(MediaType.APPLICATION_JSON_UTF8).content(invalidRequestJson))
+                .andExpect(status().is(400));
     }
 }
